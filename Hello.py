40b5ea28e009c4ebdae7afa3d269c527450f0f02
Azim Keshwani <41880113+akesh0218@users.noreply.github.com>
2025-01-24 17:33:15 -0500

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,5 +1,5 @@
-def hello_world():
-    print ("Hello, world!")
-
-
-print ("Test")
+print ("Hello, world!")
+print ("Hello, world!")
+print ("Hello, world!")
+print ("Hello, world!")
+print ("Hello, world!")
